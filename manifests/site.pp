0a48a4850378ae9945652836774ed3e844a8971e
Gabor Demjen <gabor_demjen@epam.com>
2020-04-22 14:24:23 +0200

    add nodes in site pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,18 @@
 node default {
+  file { '/root/README':
+    ensure => file,
+    content => 'Readme file ...',
+    owner => 'root',
+  }
 }
 node 'master1.puppet.vm' {
   include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
 }
